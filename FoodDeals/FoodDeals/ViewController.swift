6be69f25e3b0bb18cf1c2295e8a65816222382ce
jtchristensen <james-t-christensen@uiowa.edu>
2020-11-17 17:12:32 -0600

    First commit

---
+++ FoodDeals/FoodDeals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeals
+//
+//  Created by James Christensen on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
